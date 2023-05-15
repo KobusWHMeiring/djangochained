3ca628750d2aa2b068b52c426d5639e096b82fbf
Kobus Meiring <kobuswhmeiring@gmail.com>
2023-05-15 19:59:43 +0200

    first model up

--- djangoproj/djangoapp/models.py
+++ djangoproj/djangoapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Conversation(models.Model):
+    user = models.CharField(max_length=200)  
+    date_time = models.DateTimeField(auto_now_add=True)
+    message = models.TextField(max_length = 2000)
+    
+    def __str__(self):
+        return self.message
+
+    @classmethod
+    def get_most_recent_message(cls):
+        most_recent_message = cls.objects.latest('date_time')
+        return most_recent_message
+    
+    
+    
